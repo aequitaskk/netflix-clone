af1016fc56fd6c7025e6c2da77c3e614030a7a27
dalmedian27 <kkla1124@gmail.com>
2023-06-02 09:00:53 -0400

    implement react-hook-form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,27 @@
+"use client";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
 
-type Props = {};
+type Props = {
+  email: string;
+  password: string;
+};
 
 export const metadata: Metadata = {
   title: "Login - Netflix Clone",
   description: "Login page",
 };
 
-const LoginPage = (props: Props) => {
+const LoginPage = ({ email, password }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Props>();
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   return (
     <div className="relative flex h-screen w-full flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Image
@@ -29,14 +41,37 @@ const LoginPage = (props: Props) => {
         />
       </Link>
 
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+      <form
+        onSubmit={onSubmit}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+      >
         <h1 className="text-4xl font-semibold">Sign In</h1>
         <div className="space-y-4">
           <label className="inline-block w-full">
-            <input type="email" placeholder="Email" className="input" />
+            <input
+              type="email"
+              placeholder="Email"
+              className="input"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-sm font-light text-red-400">
+                Please enter a valid email
+              </p>
+            )}
           </label>
           <label className="inline-block w-full">
-            <input type="password" placeholder="Password" className="input" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="input"
+              {...register("password", { required: true })}
+            />
+            {errors.password && (
+              <p className="p-1 text-sm font-light text-red-400">
+                Your password must contain between 4 and 60 characters
+              </p>
+            )}
           </label>
         </div>
 
